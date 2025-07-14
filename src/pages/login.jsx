defdde82958f02feb26d999289ee4a4f69550d07
Mery30 <59580911+mimi202@users.noreply.github.com>
2025-07-14 12:16:53 +0100

    Updated login.jsx

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -143,35 +143,3 @@ function Login() {
 }
 
 export default Login;
-      navigate("/profile");
-    } catch (err) {
-      setErrors({ form: "Erreur serveur" });
-    }
-  };
-
-  return (
-    <form className="container my-5" onSubmit={handleLogin}>
-      <div className="row justify-content-center">
-        <div className="col-12 col-md-8 bg-light p-4 rounded shadow">
-          <h4 className="text-center mb-4">Connexion</h4>
-          {errors.form && <div className="text-danger">{errors.form}</div>}
-          <div className="mb-3">
-            <label className="form-label">Nom d'utilisateur :</label>
-            <input type="text" className="form-control" ref={usernameRef} />
-          </div>
-          <div className="mb-3">
-            <label className="form-label">Mot de passe :</label>
-            <input type="password" className="form-control" ref={passwordRef} />
-          </div>
-          <div className="text-center">
-            <button className="btn btn-success px-5 mt-3" type="submit">
-              Connexion
-            </button>
-          </div>
-        </div>
-      </div>
-    </form>
-  );
-}
-
-export default Login;
